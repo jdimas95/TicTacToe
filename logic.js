7ad8ec97638396a68ea675516d579069ad822f7d
Julio Dimas <juliodimas1995@live.com>
2018-12-31 13:35:17 -0800

    Version 1.0

--- logic.js
+++ logic.js
@@ -1,6 +1,7 @@
 /*global document, window, alert, console, require*/
 
 var turns = 0;
+var selected, element;
 
 var player1 = {
     isActive: false,
@@ -12,26 +13,43 @@ var player2 = {
     assignedMarker: 'x.png',
 };
 
-var availableSpaces = [];
+var marked = [];
 
 document.querySelector('#Start').addEventListener('click', initNewGame());
 
-while (turns < 10){
-if (player1.isActive === true) {
-    console.log('It\'s working');
-    turns++;
-}
-else if (player2.isActive === true){
-    console.log('It\'s not working');
-    turns++;
-}
+function marking (element) {
+    if (player1.isActive === true && turns < 10){
+        turns++;
+        document.querySelector(element).src = player1.assignedMarker;
+        toggleActive();
+    }
+    else if (player2.isActive === true && turns < 10) {
+        turns++;
+        document.querySelector(element).src = player2.assignedMarker;
+        toggleActive();
+    }
 }
 
-var test = document.querySelector('#s1').id;
-console.log(test);
+//var test = document.querySelector('#s1').id;
+//console.log(test);
 
 function initNewGame() {
+    turns = 0;
     document.querySelector('#s1, #s2, #s3, #s4, #s5, #s6, #s7, #s8, #s9').src = 'emptySpace.png';
     document.getElementById('player1').classList.add('active');
     player1.isActive = true;
+    
 }
+
+function toggleActive(){
+    if (player1.isActive === true) {
+        player1.isActive = false;
+        player2.isActive = true;
+    }
+    else if (player2.isActive === true) {
+        player2.isActive = false;
+        player1.isActive = true;
+    }
+}
+
+
